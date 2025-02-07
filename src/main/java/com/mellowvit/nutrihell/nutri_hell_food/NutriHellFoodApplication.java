e6680b67c9132a5e8ce6056e3eca496a8879d858
MellowVit <luispbp@gmail.com>
2025-02-07 12:07:14 +0100

    initial project structure

---
+++ src/main/java/com/mellowvit/nutrihell/nutri_hell_food/NutriHellFoodApplication.java
@@ -0,0 +1,13 @@
+package com.mellowvit.nutrihell.nutri_hell_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriHellFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriHellFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mellowvit/nutrihell/nutri_hell_food/NutriHellFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mellowvit.nutrihell.nutri_hell_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriHellFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
